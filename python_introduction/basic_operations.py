22e714decf27cc20b6580893c63099583221b061
mouad-labs <mouadaitbihim@gmail.com>
2025-09-10 07:19:31 +0100

    Add my Python script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+sub = number1 - number2
+assuming = number1 * number2
+print("addition of", number1, "and", number2, "is", sum,)
+print("subtraction of", number1, "and", number2, "is", sub,)
+print("multiplication of", number1, "and", number2, "is", assuming,)
